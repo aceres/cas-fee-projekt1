b74f99bcee21fe51b63a0686ad973b8a696205e7
André Ceres <andre.ceres@gmail.com>
2017-06-05 15:45:59 +0200

    Week 4
    - safer start jQuery instead od window.onload

--- javascripts/appListNote.js
+++ javascripts/appListNote.js
@@ -15,109 +15,111 @@ const btnSortByFinishDate = document.getElementById("btnSortByFinishDate");
 const btnSortByTitle = document.getElementById("btnSortByTitle");
 const btnShowAllFinishedTasks = document.getElementById("btnShowAllFinishedTasks");
 
-$(function () {
+(function($, window, document) {
+    $(function () {
 
-    const dataNote = {
-        appNote: [
-            {
-                id: 1,
-                title: "UI testen",
-                description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-                importance: 2,
-                createdDate: 1489017600000,
-                finishDate: 1492732800000,
-                finished: true
-            },
-            {
-                id: 2,
-                title: "Hausaufgaben machen",
-                description: "Hier wird die genaue Beschreibung gemacht.",
-                importance: 5,
-                createdDate: 1489017600000,
-                finishDate: 1489017600000,
-                finished: true
-            },
-            {
-                id: 3,
-                title: "Für die Prüfung lernen",
-                description: "2. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-                importance: 1,
-                createdDate: 1489017600000,
-                finishDate: 1492732800000,
-                finished: false
-            },
-            {
-                id: 4,
-                title: "Priorität 1: HTML umsetzen",
-                description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-                importance: 3,
-                createdDate: 1489017600000,
-                finishDate: 1492732800000,
-                finished: false
-            },
-            {
-                id: 5,
-                title: "Dozent fragen",
-                description: "Da wird die ungenaue Beschreibung vorgestellt.",
-                importance: 4,
-                createdDate: 1489017600000,
-                finishDate: 1495238400000,
-                finished: true
+        const dataNote = {
+            appNote: [
+                {
+                    id: 1,
+                    title: "UI testen",
+                    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+                    importance: 2,
+                    createdDate: 1489017600000,
+                    finishDate: 1492732800000,
+                    finished: true
+                },
+                {
+                    id: 2,
+                    title: "Hausaufgaben machen",
+                    description: "Hier wird die genaue Beschreibung gemacht.",
+                    importance: 5,
+                    createdDate: 1489017600000,
+                    finishDate: 1489017600000,
+                    finished: true
+                },
+                {
+                    id: 3,
+                    title: "Für die Prüfung lernen",
+                    description: "2. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+                    importance: 1,
+                    createdDate: 1489017600000,
+                    finishDate: 1492732800000,
+                    finished: false
+                },
+                {
+                    id: 4,
+                    title: "Priorität 1: HTML umsetzen",
+                    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+                    importance: 3,
+                    createdDate: 1489017600000,
+                    finishDate: 1492732800000,
+                    finished: false
+                },
+                {
+                    id: 5,
+                    title: "Dozent fragen",
+                    description: "Da wird die ungenaue Beschreibung vorgestellt.",
+                    importance: 4,
+                    createdDate: 1489017600000,
+                    finishDate: 1495238400000,
+                    finished: true
+                }
+            ]
+        };
+
+        if (localStorage.getItem("localDataNote")) {
+
+            // Get object from the localStorage
+            localStorageDataNote = JSON.parse(localStorage.getItem("localDataNote"));
+            // Pass our data to the template
+            allNotesCompiledHtml = templateAllNote(localStorageDataNote);
+        } else {
+
+            // Add object to the localStorage
+            updateLocalStorage(dataNote);
+            // Pass our data to the template
+            allNotesCompiledHtml = templateAllNote(dataNote);
+        }
+
+        // Add the compiled html to the page
+        // Display all notes
+        $("ul#listAllNote").append(allNotesCompiledHtml);
+
+        /* Accordion (Description) */
+        let charLimit = 120;
+        function truncate(el) {
+
+            let text = el.text();
+            let countText = text.trim().length;
+            if (countText < 120) {
+                el.parent().parent().next().hide();
+            } else {
+                el.attr("data-original-text", text);
+                el.text(text.substring(0, charLimit) + "...");
             }
-        ]
-    };
-
-    if (localStorage.getItem("localDataNote")) {
-
-        // Get object from the localStorage
-        localStorageDataNote = JSON.parse(localStorage.getItem("localDataNote"));
-        // Pass our data to the template
-        allNotesCompiledHtml = templateAllNote(localStorageDataNote);
-    } else {
-
-        // Add object to the localStorage
-        updateLocalStorage(dataNote);
-        // Pass our data to the template
-        allNotesCompiledHtml = templateAllNote(dataNote);
-    }
-
-    // Add the compiled html to the page
-    // Display all notes
-    $("ul#listAllNote").append(allNotesCompiledHtml);
-
-    /* Accordion (Description) */
-    let charLimit = 120;
-    function truncate(el) {
-
-        let text = el.text();
-        let countText = text.trim().length;
-        if (countText < 120) {
-            el.parent().parent().next().hide();
-        } else {
-            el.attr("data-original-text", text);
-            el.text(text.substring(0, charLimit) + "...");
         }
-    }
 
-    $(".truncated").each(function () {
-        truncate($(this));
-    });
+        $(".truncated").each(function () {
+            truncate($(this));
+        });
 
-    function reveal(el) {
-        el.text(el.attr("data-original-text"));
-    }
-
-    $("a.more").on("click", function(e) {
-        e.preventDefault();
-        if ($(this).text() === "More") {
-            $(this).text("Less");
-            reveal($(this).prev().find(".truncated"));
-        } else {
-            $(this).text("More");
-            truncate($(this).prev().find(".truncated"));
+        function reveal(el) {
+            el.text(el.attr("data-original-text"));
         }
+
+        $("a.more").on("click", function(e) {
+            e.preventDefault();
+            if ($(this).text() === "More") {
+                $(this).text("Less");
+                reveal($(this).prev().find(".truncated"));
+            } else {
+                $(this).text("More");
+                truncate($(this).prev().find(".truncated"));
+            }
+        });
     });
-});
+}(window.jQuery, window, document));
 
 // Sort by importance
 btnSortByImportance.addEventListener("click", function() {
