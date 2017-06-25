c4e4dc5503e8356b451fd220c03467d5062bd0c4
André Ceres <andre.ceres@gmail.com>
2017-06-25 11:56:21 +0200

    Week 8
    - Cleaned up style

--- public/javascripts/controller/controllerStyleNote.js
+++ public/javascripts/controller/controllerStyleNote.js
@@ -5,34 +5,24 @@ const style = (function () {
     let applyStyle = function(e) {
 
         $('body').toggleClass('changeBlackStyle');
-        saveStyle(e.currentTarget.value)
+        modelNoteProApplication.sessionKey("style", e.currentTarget.value, "setKey");
     }
 
     let loadStyle = function() {
 
         $("#selectStyle option").filter(function() {
-            return this.value == modelNoteProApplication.sessionKey("style", null, "get");
+            return this.value == modelNoteProApplication.sessionKey("style", null, "getKey");
         }).prop('selected', true);
 
-        if (modelNoteProApplication.sessionKey("style", null, "get") === "blackBg") {
+        if (modelNoteProApplication.sessionKey("style", null, "getKey") === "blackBg") {
             $('body').toggleClass('changeBlackStyle');
         } else {
             $('body').removeClass('changeBlackStyle');
         }
     }
 
-    let initializeStyle = function() {
-        modelNoteProApplication.sessionKey("style", "greyBg", "set");
-    }
-
-    let saveStyle = function(styleName) {
-        modelNoteProApplication.sessionKey("style", styleName, "set");
-    }
-
     return {
         loadStyle: loadStyle,
-        applyStyle: applyStyle,
-        saveStyle: saveStyle,
-        initializeStyle: initializeStyle
+        applyStyle: applyStyle
     };
 })();
